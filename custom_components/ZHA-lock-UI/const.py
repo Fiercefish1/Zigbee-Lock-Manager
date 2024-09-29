c526aeaed9691cd647bf7b5052997b9c09670f25
Fiercefish1 <96927664+Fiercefish1@users.noreply.github.com>
2024-09-29 15:23:46 -0700

    Update const.py

--- custom_components/ZHA-lock-UI/const.py
+++ custom_components/ZHA-lock-UI/const.py
@@ -1,3 +1,1 @@
-DOMAIN = "zha_lock_ui"
-DEFAULT_SLOT_COUNT = 5  # Define the default number of slots
-PLATFORMS = ["sensor", "binary_sensor"]  # Platforms your integration supports
+DOMAIN = "zha_lock_manager"
