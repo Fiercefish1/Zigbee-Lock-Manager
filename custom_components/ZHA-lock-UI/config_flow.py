e205b586d29cd9abc957e7c865aadd6f169871df
Fiercefish1 <96927664+Fiercefish1@users.noreply.github.com>
2024-09-29 15:23:18 -0700

    Update config_flow.py

--- custom_components/ZHA-lock-UI/config_flow.py
+++ custom_components/ZHA-lock-UI/config_flow.py
@@ -1,5 +1,10 @@
+import voluptuous as vol
 from homeassistant import config_entries
+from homeassistant.components.lock import DOMAIN as LOCK_DOMAIN
 from .const import DOMAIN
+import logging
+
+_LOGGER = logging.getLogger(__name__)
 
 class LockCodeFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle the configuration flow for Zigbee Lock Manager."""
@@ -11,12 +16,16 @@ class LockCodeFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
         errors = {}
 
         if user_input is not None:
-            # Store user input (number of slots) and create the config entry
+            _LOGGER.debug(f"User input: {user_input}")
             return self.async_create_entry(title="Zigbee Lock Manager", data=user_input)
+
+        # Fetch available lock entities
+        lock_entities = [entity.entity_id for entity in self.hass.states.async_all(LOCK_DOMAIN)]
 
         # Define the form schema
         schema = vol.Schema({
-            vol.Required("slot_count", default=1): vol.Coerce(int)
+            vol.Required("slot_count", default=1): vol.Coerce(int),
+            vol.Required("lock_name", default=lock_entities[0]): vol.In(lock_entities),
         })
 
         return self.async_show_form(step_id="user", data_schema=schema, errors=errors)
